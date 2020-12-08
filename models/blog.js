c79e12cd96837601868fa3292b573612921a3d52
chadmuro <63533886+chadmuro@users.noreply.github.com>
2020-12-08 18:32:03 +0900

    show date and remove snippet

--- models/blog.js
+++ models/blog.js
@@ -1,13 +1,8 @@
-const { truncate } = require('lodash');
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const blogSchema = new Schema({
     title: {
-        type: String,
-        required: true
-    },
-    snippet: {
         type: String,
         required: true
     },
